952030a3e610c80d10cfc676af95eff5b67a03d1
lqk <qkliang@foxmail.com>
2019-06-11 22:27:55 +0800

    first commit

---
+++ SpringMVC/src/com/atguigu/springmvc/handles/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.atguigu.springmvc.handles;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloWorld {
+	@RequestMapping("/hello")
+	public String hello(){
+		System.out.println("hello");
+		return "success";
+	}
+}
