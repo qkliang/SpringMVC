cb3f9d06aa16699a113c237f8e23934ae4f44795
lqk <qkliang@foxmail.com>
2019-06-12 21:58:42 +0800

    hiddenHttpMethodFilter

--- SpringMVC/WebContent/index.jsp
+++ SpringMVC/WebContent/index.jsp
@@ -7,6 +7,24 @@
 <title>Insert title here</title>
 </head>
 <body>
+
+	<form action="springmvc/testHiddedMethod/ei" method="post">
+		<input type="hidden" name="_method" value="delete" />
+		<input type="submit" value="testDelete"/>
+	</form>
+	<form action="springmvc/testHiddedMethod/3" method="post">
+	<input type="hidden" name="_method" value="put" />
+		<input type="submit" value="testPut"/>
+	</form>
+	<br><br>
+	
+	<form action="springmvc/testHiddedMethod/2" method="post">
+		<input type="submit" value="testpost"/>
+	</form>
+	
+	<br><br>
+	<a href="springmvc/testHiddedMethod/1">test Get</a>
+	<br><br>
 	<a href="springmvc/testVariable/123">test Variable</a>
 <br>
 <a href="springmvc/testParams?username=1&age=12">test params</a>
--- SpringMVC/src/com/atguigu/springmvc/handles/RequestMappingTest.java
+++ SpringMVC/src/com/atguigu/springmvc/handles/RequestMappingTest.java
@@ -3,12 +3,40 @@ package com.atguigu.springmvc.handles;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 
 @RequestMapping("/springmvc")
 @Controller
 public class RequestMappingTest {
 	
 	public static final String SUCCESS = "success";
+	
+	@RequestMapping(value="/testHiddedMethod/{id}",method=RequestMethod.DELETE)
+	public String testHiddenDelete(@PathVariable("id") String id){
+		System.out.println("testHiddenDelete : " + id);
+		return SUCCESS;
+	}
+	
+	@RequestMapping(value = "/testHiddedMethod/{id}",method = RequestMethod.PUT)
+	public String testHiddenPut(@PathVariable("id") Integer id){
+		System.out.println("testHiddenPut : " + id);
+		return SUCCESS;
+	}
+	
+	@RequestMapping(value = "/testHiddedMethod/{id}",method = RequestMethod.POST)
+	public String testHiddenPost(@PathVariable("id") Integer id){
+		
+		System.out.println("testHiddenPost : " + id);
+		return SUCCESS;
+	}
+	
+	@RequestMapping("/testHiddedMethod/{id}")
+	public String testHiddenGet(@PathVariable("id") Integer id){
+		System.out.println("testHiddenGet : " + id);
+		return SUCCESS;
+	}
+	
 	
 	@RequestMapping("/testVariable/{id}")
 	public String testVariable(@PathVariable("id") Integer id){
